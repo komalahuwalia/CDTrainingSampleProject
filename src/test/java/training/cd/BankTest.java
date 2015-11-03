45c3fd2c25dc5e0dc8ef0abef60a0775a9c49f08
Vivek Jain <vivekjain10@gmail.com>
2015-11-03 12:09:22 +0530

    Testing createAccount with null person

--- src/test/java/training/cd/BankTest.java
+++ src/test/java/training/cd/BankTest.java
@@ -1,7 +1,21 @@
 package training.cd;
 
-import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class BankTest {
+  private Bank bank;
 
+  @Before
+  public void setUp() throws Exception {
+    bank = new Bank();
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void shouldNotCreateAccountWhenPersonIsNull() throws Exception {
+    bank.createAccount(null);
+  }
 }
