e601197bc4304aa41bf9b3ce48e051d21a4006c6
Vivek Jain <vivekjain10@gmail.com>
2015-11-03 17:14:57 +0530

    Inject dependencies to Bank to enable use to TestDoubles. Unit test creation of an account.

--- src/main/java/training/cd/Bank.java
+++ src/main/java/training/cd/Bank.java
@@ -7,16 +7,25 @@ import training.cd.repository.Database;
 import training.cd.repository.RedisDatabase;
 
 public class Bank {
+  private Database database;
+  private Logger logger;
+
+  public Bank() {
+    this(new RedisDatabase(), new Logger());
+  }
+
+  public Bank(Database database, Logger logger) {
+    this.database = database;
+    this.logger = logger;
+  }
 
   public Account createAccount(Person person) {
     if (person == null) throw new RuntimeException("Person cannot be null");
 
-    Database database = new RedisDatabase();
-
     Account account = new Account(0.0, person);
     database.create(account);
 
-    new Logger().debug(String.format("account created with id: %d", account.id));
+    logger.debug(String.format("account created with id: %d", account.id));
 
     return account;
   }
@@ -28,18 +37,18 @@ public class Bank {
   public void deposit(double amount, Account account) {
     account.balance += amount;
 
-    new Logger().debug(String.format("deposited %s to %d", amount, account.id));
+    logger.debug(String.format("deposited %s to %d", amount, account.id));
 
-    new RedisDatabase().update(account);
+    database.update(account);
   }
 
   public boolean withdraw(double amount, Account account) {
     if (account.balance < amount) return false;
 
     account.balance -= amount;
-    new RedisDatabase().update(account);
+    database.update(account);
 
-    new Logger().debug(String.format("withdrew %s from %d", amount, account.id));
+    logger.debug(String.format("withdrew %s from %d", amount, account.id));
 
     return true;
   }
@@ -55,7 +64,7 @@ public class Bank {
       return false;
     }
 
-    new Logger().debug(String.format("transferred %s from %d to %d", amount, from.id, to.id));
+    logger.debug(String.format("transferred %s from %d to %d", amount, from.id, to.id));
 
     return true;
   }
--- src/test/java/training/cd/BankTest.java
+++ src/test/java/training/cd/BankTest.java
@@ -3,19 +3,39 @@ package training.cd;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import training.cd.infrastructure.Logger;
+import training.cd.model.Account;
+import training.cd.model.Person;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BankTest {
+  @Mock
+  private Logger logger;
+
+  private TestDatabase database;
   private Bank bank;
 
   @Before
   public void setUp() throws Exception {
-    bank = new Bank();
+    database = new TestDatabase();
+    bank = new Bank(database, logger);
   }
 
   @Test(expected = RuntimeException.class)
   public void shouldNotCreateAccountWhenPersonIsNull() throws Exception {
     bank.createAccount(null);
+  }
+
+  @Test
+  public void shouldCreateAccount() throws Exception {
+    Person person = new Person(1, "Some User");
+    Account account = bank.createAccount(person);
+
+    assertThat(account.owner.id, is(1L));
   }
 }
